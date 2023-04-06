919e311ec007e9e7aa34c8e996646d4da58e6fd6
Gonxos <gonjibra@gmail.com>
2023-04-06 14:05:07 +0200

    add test modification

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "default message cambio test devops";
+ 	    String meessage= "hago prueba con webhook";
  	    Assert.assertNotNull(meessage);
  }
  
